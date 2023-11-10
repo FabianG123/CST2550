91b33be05b88f9c8d2612a53c4e0fb04cd9abdad
FabianG123 <galaselfabian@gmail.com>
2023-11-10 10:06:33 +0000

    i added this

--- functions_lab2.cpp
+++ functions_lab2.cpp
@@ -1,4 +1,4 @@
-#ios <stream>
+#include <iostream>
 using namespace std;
 
 
@@ -8,4 +8,14 @@ int max (int x, int y)
     return x;
   else
     return y;
+}
+
+int main()
+{
+  int a = 30, b = 20;
+
+  int m = max(a,b);
+
+  cout << " m is " << m << endl;
+  return 0;
 }
